825ad105dbcdf1e9336489e4ea655a5839998499
Paulius <paulius@2simple.com>
2018-02-01 17:35:41 +0000

    add all files

---
+++ src/components/test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>test component</div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
