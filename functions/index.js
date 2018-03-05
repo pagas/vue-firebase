242eb4658d050a5340925e349bac4b8452f8e2e7
paulius <paulius@2simple.com>
2018-03-05 23:14:05 +0000

    ability to run vue from node server

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,80 @@
 const functions = require('firebase-functions');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    debugger;
+    response.send('Hello World!');
 });
 
-exports.iLoveMyWife = functions.https.onRequest((request, response) => {
-    response.send("<p>I love my wife!</p><img src='http://cdn2.momjunction.com/wp-content/uploads/2017/11/Love-Quotes-For-Wife5.jpg'>");
-});
-
-exports.getMessages = functions.https.onRequest((request, response) => {
-    if (request.method === 'GET') {
-        response.status(200).send('Hello World!');
-    }
-});
-
-exports.getConversations = functions.https.onRequest((request, response) => {
-    if (request.method === 'GET') {
-
-        response.status(200).send('Hello World!');
-    }
-});
-
-
+// const functions = require('firebase-functions');
+// var admin = require("firebase-admin");
+// admin.initializeApp(functions.config().firebase);
+//
+// function respondJson(response, output) {
+//     response.status(200).send(JSON.stringify(output, null, 3));
+// }
+//
+// exports.getMessages = functions.https.onRequest((request, response) => {
+//     if (request.method === 'GET') {
+//         response.status(200).send('Hello World!');
+//     }
+// });
+//
+//
+// exports.getConversation = functions.https.onRequest((request, response) => {
+//     console.log('query:', request.query);
+//     if (request.method === 'GET' && request.query.conversationId) {
+//         var conversationId = request.query.conversationId;
+//
+//
+//
+//         admin.firestore().collection('conversation').doc(conversationId).get()
+//             .then(doc => {
+//
+//                 respondJson(response, {test:'est'});
+//                 return false;
+//                // return doc.data();
+//             })
+//             .then(conversation => {
+//                 let usersPromises = [];
+//
+//                 //respondJson(response, conversation);
+//                 return true;
+//
+//                 // for (let userId in conversation.userIds) {
+//                 //     usersPromises.push(admin.database().ref('users/' + userId).once('value')
+//                 //         .then(snapshot => {
+//                 //             return snapshot.val();
+//                 //         })
+//                 //     );
+//                 // }
+//                 // return Promise.all(usersPromises).then(users => {
+//                 //     let formatedUsers = users.map(user => {
+//                 //         return {id: user.id, name: user.name}
+//                 //     });
+//                 //     conversation.users = formatedUsers;
+//                 //     return conversation;
+//                 // });
+//             })
+//             // .then(conversation => {
+//             //     response.status(200).send(conversation);
+//             //     res.setHeader('Content-Type', 'application/json');
+//             //     res.send(JSON.stringify({ a: 1 }, null, 3));
+//             //     return true;
+//             // })
+//             .catch(error => {
+//                 console.log(error);
+//             });
+//
+//     } else {
+//         response.status(500).send({error: 'Incorrect GET request'});
+//     }
+// });
+//
+// //
+// // exports.getConversations = functions.https.onRequest((request, response) => {
+// //     if (request.method === 'GET') {
+// //         response.status(200).send('Hello World!');
+// //     }
+// // });
+//
+//
