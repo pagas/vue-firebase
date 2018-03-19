4ce8dc7a200eb4ce473181b6d8761340a2e78853
paulius <paulius@2simple.com>
2018-03-19 22:35:55 +0000

    move routers into separate file

--- server/app.js
+++ server/app.js
@@ -1,7 +1,5 @@
 'use strict';
-var Promise = require("bluebird");
-var admin = require("./helpers/firebaseAdmin");
-var conversationService = require("./services/conversation.service");
+var conversation = require('./routes/conversations');
 
 var http = require("http"),
     pathUtils = require("path"),
@@ -14,40 +12,8 @@ app.use(express.static(appDir));
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
-
-app.post("/createConversation", function (req, res) {
-    var conversation = req.body.conversation;
-    var user = req.body.user;
-    conversationService.createConversation(conversation, user)
-        .then(response => {
-            res.json(response);
-        });
-});
-
-/**
- * Flag conversation as deleted as well appropriate userConversation records.
- */
-app.get("/removeConversation", function (req, res) {
-    var conversationId = req.query.conversationId;
-    var userId = req.query.userId;
-    var collectionPromises = [];
-    collectionPromises.push(admin.firestore().collection('conversations')
-        .doc(conversationId).update({deleted: true})
-    );
-    collectionPromises.push(admin.firestore().collection('userConversations')
-        .where('userId', '==', userId)
-        .where('conversationId', '==', conversationId)
-        .get().then(function (querySnapshot) {
-            querySnapshot.forEach(function (doc) {
-                doc.ref.update({deleted: true});
-            });
-        })
-    );
-
-    Promise.all(collectionPromises).then(response => {
-        res.json(response);
-    })
-});
+// register conversation routes
+app.use(conversation);
 
 app.get("*", function (req, res) {
     res.sendfile(pathUtils.resolve(appDir, "index.html"));
---
+++ server/routes/conversations.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var Promise = require("bluebird");
+var admin = require("../helpers/firebaseAdmin");
+var conversationService = require("../services/conversation.service");
+
+router.post("/createConversation", function (req, res) {
+    var conversation = req.body.conversation;
+    var user = req.body.user;
+    conversationService.createConversation(conversation, user)
+        .then(response => {
+            res.json(response);
+        });
+});
+
+/**
+ * Flag conversation as deleted as well appropriate userConversation records.
+ */
+router.get("/removeConversation", function (req, res) {
+    var conversationId = req.query.conversationId;
+    var userId = req.query.userId;
+    var collectionPromises = [];
+    collectionPromises.push(admin.firestore().collection('conversations')
+        .doc(conversationId).update({deleted: true})
+    );
+    collectionPromises.push(admin.firestore().collection('userConversations')
+        .where('userId', '==', userId)
+        .where('conversationId', '==', conversationId)
+        .get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                doc.ref.update({deleted: true});
+            });
+        })
+    );
+
+    Promise.all(collectionPromises).then(response => {
+        res.json(response);
+    })
+});
+
+module.exports = router;
